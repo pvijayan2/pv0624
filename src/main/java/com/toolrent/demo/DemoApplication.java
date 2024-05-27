fd33d5aade6f796e5020b72e977b0cbf75aa4f9e
Priya <vijayan.priya@gmail.com>
2024-05-27 13:36:50 -0400

    Initial comment

---
+++ src/main/java/com/toolrent/demo/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.toolrent.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
